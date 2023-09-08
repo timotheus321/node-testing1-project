16e5d631f33817db3c6ca79bc6b14cc8717ee60c
Timothy White <bigtball321@yahoo.com>
2023-09-07 21:08:49 -0400

    wip

--- index.js
+++ index.js
@@ -183,8 +183,9 @@ class Car {
  *    // result is false
  * })
  */
-function isEvenNumberAsync(number) {
-  // ✨ implement
+async function isEvenNumberAsync(number) {
+   return number % 2 === 0 || false
+ 
 }
 
 module.exports = {
