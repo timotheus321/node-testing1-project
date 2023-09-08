a89b5df1692359b7b130495f9137b0b35feec10d
Timothy White <bigtball321@yahoo.com>
2023-09-07 20:52:06 -0400

    wip

--- index.js
+++ index.js
@@ -71,7 +71,7 @@ class Counter {
    * counter.countDown() // returns 0
    */
   countDown() {
-    
+
     return this.count > 0 ? this.count-- : 0
 
   }
@@ -82,7 +82,8 @@ class Seasons {
    * [Exercise 5A] Seasons creates a seasons object
    */
   constructor() {
-    // ✨ initialize whatever properties are needed
+   this.season = ['summer', 'fall', 'winter', 'spring']
+   this.currentSeason = 0
   }
 
   /**
@@ -98,7 +99,13 @@ class Seasons {
    * seasons.next() // returns "summer"
    */
   next() {
-    // ✨ implement
+    const result = this.season[this.currentSeason]
+    if (this.currentSeason === 3) {
+      this.currentSeason = 0
+    } else {
+      ++this.currentSeason
+    }
+    return result 
   }
 }
 
@@ -112,7 +119,8 @@ class Car {
   constructor(name, tankSize, mpg) {
     this.odometer = 0 // car initilizes with zero miles
     this.tank = tankSize // car initiazes full of gas
-    // ✨ initialize whatever other properties are needed
+    this.tankSize = tankSize
+    this.mpg = mpg
   }
 
   /**
@@ -129,7 +137,15 @@ class Car {
    * focus.drive(200) // returns 600 (ran out of gas after 100 miles)
    */
   drive(distance) {
-    // ✨ implement
+    const milesCanDrive = this.tank * this.mpg
+    if ( distance <= milesCanDrive) {
+      this.odometer = this.odometer + distance
+      this.tank = this.tank - (distance / this.mpg)
+    return this.odometer
+    }
+    this.odometer = this.odometer + milesCanDrive
+    this.tank = 0;
+    return this.odometer
   }
 
   /**
